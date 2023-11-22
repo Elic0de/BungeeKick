d674e796a9679012c925cee85417f0533a3cf38e
oieii <lpondboy@gmail.com>
2023-11-22 21:00:16 +0900

    feat　Detect and automatically ban alt accounts

--- src/main/java/net/japanpvpserver/bungeekick/BungeeKick.java
+++ src/main/java/net/japanpvpserver/bungeekick/BungeeKick.java
@@ -2,20 +2,31 @@ package net.japanpvpserver.bungeekick;
 
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
+import com.mrpowergamerbr.temmiewebhook.DiscordEmbed;
 import lombok.Getter;
+import me.leoko.advancedban.Universal;
+import me.leoko.advancedban.bungee.event.PunishmentEvent;
+import me.leoko.advancedban.manager.PunishmentManager;
+import me.leoko.advancedban.manager.TimeManager;
+import me.leoko.advancedban.utils.Punishment;
+import me.leoko.advancedban.utils.PunishmentType;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.config.ServerInfo;
+import net.md_5.bungee.api.connection.PendingConnection;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.event.LoginEvent;
 import net.md_5.bungee.api.event.ServerKickEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.event.EventHandler;
 import net.william278.annotaml.Annotaml;
 
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Objects;
 import java.util.logging.Level;
 
 public final class BungeeKick extends Plugin implements Listener {
@@ -30,14 +41,29 @@ public final class BungeeKick extends Plugin implements Listener {
         getProxy().getPluginManager().registerListener(this, this);
     }
 
-    private void loadConfig() throws RuntimeException {
-        try {
-            this.settings = Annotaml.create(new File(getDataFolder(), "config.yml"), BungeeKickSettings.class).get();
-        } catch (IOException | InvocationTargetException | InstantiationException |
-                 IllegalAccessException e) {
-            getLogger().log(Level.SEVERE, "Failed to load configuration files", e);
-            throw new RuntimeException(e);
+    @EventHandler
+    private void onLogin(LoginEvent event) {
+        final Punishment punishment = PunishmentManager.get().getBan(event.getConnection().getUniqueId().toString());
+        if (punishment == null) return;
+        for (final String ipAddress : Universal.get().getIps().values()) {
+            if (Objects.equals(ipAddress, event.getConnection().getAddress().getAddress().getHostAddress())) {
+                final PendingConnection connection = event.getConnection();
+                final String name = connection.getName();
+                final String uuid = connection.getUniqueId().toString();
+                final String reason = "Alt Account Automatically detected";
+                final String operation = "Console";
+
+                new Punishment(name, uuid, reason, operation, PunishmentType.BAN, TimeManager.getTime(), -1, null, -1).create();
+                PunishmentManager.get().discard(name);
+            }
         }
+    }
+
+    @EventHandler
+    private void onPunishment(PunishmentEvent event) {
+        final Punishment punishment = event.getPunishment();
+        final String duration = punishment.getDuration(true).equals("permanent") ? "永久" : punishment.getDuration(true);
+        DiscordEmbed.builder().color(Color.RED.getRGB()).description(String.format("名前: ``%s(%s)``\n理由: %s\n期限: %s", punishment.getName(), punishment.getUuid(), punishment.getReason(), duration));
     }
 
     @EventHandler
@@ -54,6 +80,16 @@ public final class BungeeKick extends Plugin implements Listener {
             return;
         }
         connectSection(player, name);
+    }
+
+    private void loadConfig() throws RuntimeException {
+        try {
+            this.settings = Annotaml.create(new File(getDataFolder(), "config.yml"), BungeeKickSettings.class).get();
+        } catch (IOException | InvocationTargetException | InstantiationException |
+                 IllegalAccessException e) {
+            getLogger().log(Level.SEVERE, "Failed to load configuration files", e);
+            throw new RuntimeException(e);
+        }
     }
 
     private void connectSection(ProxiedPlayer player, String name) {
